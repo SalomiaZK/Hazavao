dd5cdd4e7e166b8e0c52e17f261be3695ab6e532
SalomiaZK <153986890+SalomiaZK@users.noreply.github.com>
2025-06-13 09:31:40 +0300

    feat: openAI service in HazavaoController.java

--- src/main/java/com/salomia/zk/endpoint/rest/controller/HazavaoController.java
+++ src/main/java/com/salomia/zk/endpoint/rest/controller/HazavaoController.java
@@ -1,12 +1,16 @@
 package com.salomia.zk.endpoint.rest.controller.health;
 
+import com.salomia.zk.service.HazavaoService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HazavaoController {
+    private HazavaoService hazavaoService;
+
     @GetMapping("/hazavao")
-    public String hazavao() {
-        return "hazavao";
+    public String hazavao(@RequestParam String word) {
+        return String.valueOf(hazavaoService.getChatCompletion("donne la definition du mot" + word));
     }
 }
