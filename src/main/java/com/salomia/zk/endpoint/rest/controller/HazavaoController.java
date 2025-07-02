bec6bb947cda87de9f3d4a850634bd7ad7775745
SalomiaZK <salomia.razakarivony@gmail.com>
2025-07-02 20:50:32 +0300

    feat: type

--- src/main/java/com/salomia/zk/endpoint/rest/controller/HazavaoController.java
+++ src/main/java/com/salomia/zk/endpoint/rest/controller/HazavaoController.java
@@ -1,10 +1,11 @@
-package com.salomia.zk.endpoint.rest.controller.health;
+package com.salomia.zk.endpoint.rest.controller;
 
 import com.salomia.zk.service.HazavaoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class HazavaoController {
@@ -12,7 +13,7 @@ public class HazavaoController {
     private HazavaoService hazavaoService;
 
     @GetMapping("/hazavao")
-    public String hazavao(@RequestParam String word) {
-        return String.valueOf(hazavaoService.getChatCompletion("donne la definition du mot" + word));
+    public Mono<String> hazavao(@RequestParam String word) {
+        return hazavaoService.getChatCompletion("donne la definition du mot" + word);
     }
 }
